b65774856d277480e79f4bb76ac84b2a66397512
Bhagya Patel <bhagyapatel1971@gmail.com>
2025-03-09 16:48:44 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Change this to your frontend URL
+    origin: "http://localhost:3000", // Change this to your frontend URL
     credentials: true, // Allow cookies
   })
 );
